c034abb6808457e463b0813c34aaee7592d5a3e7
Kefkius <kefkius@mail.com>
2014-12-17 13:03:04 -0500

    rename script

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         tasks: ['markdown']
       },
       js: {
-        files: ['Gruntfile.js', 'insight.js', 'app/**/*.js'],
+        files: ['Gruntfile.js', 'clamsight.js', 'app/**/*.js'],
         options: {
           livereload: true,
         },
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
     },
     jshint: {
       all: {
-        src: ['Gruntfile.js', 'insight.js', 'app/**/*.js', 'lib/*.js', 'config/*.js'],
+        src: ['Gruntfile.js', 'clamsight.js', 'app/**/*.js', 'lib/*.js', 'config/*.js'],
         options: {
           jshintrc: true
         }
@@ -45,7 +45,7 @@ module.exports = function(grunt) {
     },
     nodemon: {
       dev: {
-        script: 'insight.js',
+        script: 'clamsight.js',
         options: {
           args: [],
           ignore: ['test/**/*', 'util/**/*', 'dev-util/**/*'],
